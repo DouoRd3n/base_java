f3a86b5e1408a81f8523f4d29a5b0fa9da0e29df
DouORd3n <DouORd3n@gmail.com>
2021-04-03 18:52:33 +0300

    Lesson05 HW04

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -3,8 +3,6 @@ package ru.javawebinar.basejava.storage;
 public class ArrayStorageTest extends AbstractArrayStorageTest {
 
     public ArrayStorageTest() {
-
-
         super(new ArrayStorage());
     }
 
