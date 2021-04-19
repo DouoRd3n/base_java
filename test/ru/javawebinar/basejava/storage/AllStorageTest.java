f26670bdf16ca6e5d0e8e60502da8cb2a0b1ca66
DouORd3n <DouORd3n@gmail.com>
2021-04-19 23:00:32 +0300

    Lesson10 HW09

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -11,7 +11,8 @@ import org.junit.runners.Suite;
                 ListStorageTest.class,
                 MapUuidStorageTest.class,
                 MapResumeStorageTest.class,
-                ObjectPathStorageTest.class
+                ObjectPathStorageTest.class,
+                ObjectFileStorageTest.class
         })
 public class AllStorageTest {
 }
---
+++ test/ru/javawebinar/basejava/storage/ObjectFileStorageTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.storage.serialize.ObjectStreamSerializer;
+
+public class ObjectFileStorageTest extends AbstractStorageTest{
+    public ObjectFileStorageTest() {
+        super(new FileStorage(STORAGE_DIR.getAbsoluteFile(), new ObjectStreamSerializer()));
+    }
+}
