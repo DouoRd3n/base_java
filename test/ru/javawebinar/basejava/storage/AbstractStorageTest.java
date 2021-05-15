f02df96cfe41032faa5e04218e5234ae7b49209d
DouORd3n <DouORd3n@gmail.com>
2021-05-15 16:02:32 +0300

    Lesson11

--- test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
@@ -15,7 +15,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractStorageTest {
-    protected static final File STORAGE_DIR = new File("C:\\Users\\Serg\\IdeaProjects\\base_java\\storage");
+    protected static final File STORAGE_DIR = new File("C:\\projects\\storage");
+
 
     protected Storage storage;
 
