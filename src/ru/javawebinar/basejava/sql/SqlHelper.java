cc5935a91f1f6071b7320b016529b14fb43a1da6
DouORd3n <DouORd3n@gmail.com>
2021-05-23 12:15:09 +0300

    hw sqlStorage

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -1,5 +1,26 @@
 package ru.javawebinar.basejava.sql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SqlHelper {
-    
+    private ConnectionFactory connectionFactory;
+
+    public SqlHelper(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+    public void execute(String sql){
+        execute(sql, ps-> ps.execute());
+    }
+
+    public <T> T execute(String sql, SqlExecutor<T> executor){
+        try(Connection conn = connectionFactory.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)) {
+            return executor.execute(ps);
+        } catch (SQLException e) {
+
+        }
+        return null;
+    }
 }
--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -4,63 +4,65 @@ import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.sql.ConnectionFactory;
+import ru.javawebinar.basejava.sql.SqlHelper;
 
 import java.sql.*;
 import java.util.List;
 
 public class SqlStorage implements Storage {
-    public final ConnectionFactory connectionFactory;
+    public final SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
-        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        this.sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
     @Override
     public void clear() {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume")) {
-            ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        sqlHelper.execute("DELETE FROM resume");
     }
 
     @Override
     public Resume get(String uuid) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
+        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid=?", ps -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
             return new Resume(uuid, rs.getString("full_name"));
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
+
     }
 
     @Override
     public void update(Resume r) {
-
+        sqlHelper.execute("UPDATE resume SET (uuid, full_name) VALUES (?,?)", ps -> {
+            ps.setString(1,r.getUuid());
+            ps.setString(2,r.getFullName());
+            if (ps.executeUpdate() == 0){
+                throw new NotExistStorageException(r.getUuid());
+            }
+            return null;
+        } );
     }
 
     @Override
     public void save(Resume r) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
+        sqlHelper.execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)", ps -> {
             ps.setString(1, r.getUuid());
-            ps.setString(2, r.getFullName());
-            ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+            ps.setString(2,r.getFullName());
+
+           return null;
+        });
 
     }
 
     @Override
     public void delete(String uuid) {
-
+        sqlHelper.execute("DELETE * FROM resume WHERE uuid = ?", ps -> {
+            ps.setString(1, uuid);
+           return null;
+        });
     }
 
     @Override
