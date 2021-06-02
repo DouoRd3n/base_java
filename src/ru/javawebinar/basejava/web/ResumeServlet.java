05f8601bd5c210bf0d6de0c06738ef3670c4dfb3
DouORd3n <DouORd3n@gmail.com>
2021-06-02 18:32:51 +0300

    Lesson17 HW16

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,10 +1,10 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
-import ru.javawebinar.basejava.model.ContactType;
-import ru.javawebinar.basejava.model.Resume;
-import ru.javawebinar.basejava.model.SectionType;
+import ru.javawebinar.basejava.model.*;
 import ru.javawebinar.basejava.storage.Storage;
+import ru.javawebinar.basejava.util.DateUtil;
+import ru.javawebinar.basejava.util.HtmlUtil;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -38,14 +40,38 @@ public class ResumeServlet extends HttpServlet {
             }
         }
         for (SectionType type : SectionType.values() ) {
-            String parameter = request.getParameter(type.name());
-            switch (parameter){
-                case "PERSONAL":
-                case "OBJECTIVE":
-                case "ACHIEVEMENT":
-                case "QUALIFICATIONS":
-                case"EXPERIENCE":
-                case "EDUCATION" :
+            String value = request.getParameter(type.name());
+            String[] values = request.getParameterValues(type.name());
+            switch (type){
+                case PERSONAL:
+                case OBJECTIVE:
+                    r.addSection(type, new TextSection(value));
+                    break;
+                case ACHIEVEMENT:
+                case QUALIFICATIONS:
+                    r.addSection(type, new ListSection(value.split("\\n")));
+                    break;
+                case EXPERIENCE:
+                case EDUCATION :
+                    List<Organization> orgs = new ArrayList<>();
+                    String[] urls = request.getParameterValues(type.name() + "url");
+                    for (int i = 0; i < values.length ; i++) {
+                        String name = values[i];
+
+                            List<Organization.Position> positions = new ArrayList<>();
+                            String pfx = type.name() + i;
+                            String[] starDate = request.getParameterValues(pfx + "startDate");
+                            String[] endDate = request.getParameterValues(pfx + "endDate");
+                            String[] title = request.getParameterValues(pfx + "title");
+                            String[] description = request.getParameterValues(pfx + "description");
+                            for (int j = 0; j < title.length; j++) {
+                                if (HtmlUtil.isEmpty(name)) {
+                                positions.add(new Organization.Position(DateUtil.parse(starDate[j]), DateUtil.parse(endDate[j]), title[j], description[j]));
+                            }
+                        }
+                        orgs.add(new Organization(new Link(name, urls[i]),positions));
+                    }
+
             }
 
         }
@@ -68,8 +94,24 @@ public class ResumeServlet extends HttpServlet {
                 response.sendRedirect("resume");
                 return;
             case "view":
+                r = storage.get(uuid);
             case "edit":
                 r = storage.get(uuid);
+                for (SectionType type : new SectionType[]{SectionType.EXPERIENCE, SectionType.EDUCATION}) {
+                    OrganizationSection section = (OrganizationSection) r.getSection(type);
+                    List<Organization> emptyFirstOrganizations = new ArrayList<>();
+                    emptyFirstOrganizations.add(Organization.EMPTY);
+                    if (section != null) {
+                        for (Organization org : section.getOrganizations()) {
+                            List<Organization.Position> emptyFirstPositions = new ArrayList<>();
+                            emptyFirstPositions.add(Organization.Position.EMPTY);
+                            emptyFirstPositions.addAll(org.getPositions());
+                            emptyFirstOrganizations.add(new Organization(org.getHomePage(), emptyFirstPositions));
+                        }
+                    }
+                    r.addSection(type, new OrganizationSection(emptyFirstOrganizations));
+                }
+                
                 break;
             default:
                 throw new IllegalArgumentException("Action " + action + " is illegal");
--- web/WEB-INF/jsp/edit.jsp
+++ web/WEB-INF/jsp/edit.jsp
@@ -2,6 +2,7 @@
 <%@ page import="ru.javawebinar.basejava.model.SectionType" %>
 <%@ page import="ru.javawebinar.basejava.model.ListSection" %>
 <%@ page import="ru.javawebinar.basejava.model.OrganizationSection" %>
+<%@ page import="ru.javawebinar.basejava.util.DateUtil" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
@@ -46,9 +47,45 @@
                 <c:when test="${type == 'EXPERIENCE' || type == 'EDUCATION'}">
                     <c:forEach var="org" items="<%=((OrganizationSection)section).getOrganizations()%>"
                         varStatus="counter">
-
+                <dl>
+                    <dt>Название учереждения:</dt>
+                    <dd><input type="text" name='${type}' size=100 value="${org.homePage.name}"></dd>
+                </dl>
+                <dl>
+                    <dt>Сайт учереждения:</dt>
+                    <dd><input type="text" name='${type}url' size=100 value="${org.homePage.url}"></dd>
+                    </dd>
+                </dl>
+                <br>
+                <c style="margin-left: 30px">
+                    <c:forEach var="pos" items="${org.positions}">
+                        <jsp:useBean id="pos" type="ru.javawebinar.basejava.model.Organization.Position"/>
+                    <dl>
+                        <dt>Начальная дата:</dt>
+                        <dd>
+                            <input type="text" name="${type}${counter.index}startDate" size=10
+                                   value="<%=DateUtil.format(pos.getStartDate())%>" placeholder="MM/yyyy">
+                        </dd>
+                    </dl>
+                    <dl>
+                        <dt>Конечная дата:</dt>
+                        <dd>
+                            <input type="text" name="${type}${counter.index}endDate" size=10
+                                   value="<%=DateUtil.format(pos.getEndDate())%>" placeholder="MM/yyyy">
+                    </dl>
+                    <dl>
+                        <dt>Должность:</dt>
+                        <dd><input type="text" name='${type}${counter.index}title' size=75
+                                   value="${pos.title}">
+                    </dl>
+                    <dl>
+                        <dt>Описание:</dt>
+                        <dd><textarea name="${type}${counter.index}description" rows=5
+                                      cols=75>${pos.description}</textarea></dd>
+                    </dl>
 
                     </c:forEach>
+                        </c:forEach>
                 </c:when>
 
             </c:choose>
